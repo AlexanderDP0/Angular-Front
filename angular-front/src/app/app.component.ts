59dbe10ea182c3562e3cd41300ac5361db3d6d04
AlexanderDP0 <alexanderdvicius@gmail.com>
2024-09-10 10:56:36 -0600

    upload(NM): main navbar

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    HomeComponent,
+    UserComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-front';
+  text: any;
 }
